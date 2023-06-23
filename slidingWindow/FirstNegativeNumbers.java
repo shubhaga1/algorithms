769870ec5bc2f855861829eeb6297fabf148b442
Shubham Garg <schmuck21@gmail.com>
2023-06-23 14:12:55 +0530

    Create FirstNegativeNumbers.java

---
+++ slidingWindow/FirstNegativeNumbers.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+
+public class FirstNegativeNumbers {
+
+    public static ArrayList<Integer> findFirstNegativeNumbers(int[] arr, int k) {
+        ArrayList<Integer> result = new ArrayList<>(); // ArrayList to store the first negative numbers in each window
+        ArrayList<Integer> ans = new ArrayList<>(); // ArrayList to store the final result
+        
+        int i = 0; // Start of the window
+        int j = 0; // End of the window
+        
+        while (j < arr.length) {
+            if (arr[j] < 0) {
+                result.add(arr[j]); // Add the negative number to the result
+            }
+            j++;
+            
+            if (j - i + 1 >= k) {
+                ans.add(result.get(0)); // Add the first negative number to the final result
+                i++;
+                j++;
+                
+                if (!result.isEmpty() && arr[j] == result.get(0)) {
+                    result.remove(0); // Remove the first negative number from the result if it is the next element in the window
+                }
+            }
+        }
+        
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {2, -3, 4, -1, -2, 1, 5, -3};
+        int k = 3;
+
+        ArrayList<Integer> result = findFirstNegativeNumbers(arr, k);
+        System.out.println("First negative numbers in each window:");
+        for (int num : result) {
+            System.out.print(num + " ");
+        }
+    }
+}
