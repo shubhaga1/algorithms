090ed1a8e1c49f8861deed3b15f97c1f54ce6233
Shubham Garg <schmuck21@gmail.com>
2023-07-05 16:45:57 +0530

    Create MaxSubArray.java

---
+++ MaxSubArray.java
@@ -0,0 +1,22 @@
+// leetcode problem - Kadanes Algo - https://leetcode.com/problems/maximum-subarray/description/
+
+class MaxSubArray {
+    public int maxSubArray(int[] nums) {
+        int maxSum = Integer.MIN_VALUE;
+        int currentSum = 0;
+        
+        for (int i = 0; i < nums.length; i++) {
+            currentSum += nums[i];
+            
+            if (currentSum > maxSum) {
+                maxSum = currentSum;
+            }
+            
+            if (currentSum < 0) {
+                currentSum = 0;
+            }
+        }
+        
+        return maxSum;
+    }
+}
